744f612584a2d3dc4d089ff0b5b508a297beb1e4
alex karpovich <aakarpovich@mtt.ru>
2025-01-08 00:53:21 +0300

    add lesson 3

---
+++ lesson 3/main.py
@@ -0,0 +1,18 @@
+def main():
+    # Исходные данные
+    items = {
+        'milk15': {'name': 'молоко 1.5%', 'count': 34, 'price': 89.9},
+        'cheese': {'name': 'сыр молочный 1 кг.', 'count': 12, 'price': 990.9},
+        'sausage': {'name': 'колбаса 1 кг.', 'count': 122, 'price': 1990.9}
+    }
+
+    # Формируем новый словарь с использованием dict comprehension
+    price_less_20 = {key: (value['count'] < 20) for key, value in items.items()}
+
+    # Выводим результат
+    print(price_less_20)
+
+
+
+if __name__ == "__main__":
+    main()
